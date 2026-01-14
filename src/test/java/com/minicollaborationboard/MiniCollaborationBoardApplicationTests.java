5d57a9e318583677418819963d5c485d15fbc26c
JangSuhyeon <shjang0043@gmail.com>
2026-01-14 14:17:58 +0900

    Initial commit

---
+++ src/test/java/com/minicollaborationboard/MiniCollaborationBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minicollaborationboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCollaborationBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
